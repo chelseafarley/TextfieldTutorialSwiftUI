0675f545a849dcceb647e5a173a5e5f54d52190b
Chelsea <chelsea@tripwiretech.com>
2023-07-20 21:21:16 +1200

    Initial Commit

---
+++ TextInputTutorial/TextInputTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInputTutorialApp.swift
+//  TextInputTutorial
+//
+//  Created by Charles Suddens on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TextInputTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
